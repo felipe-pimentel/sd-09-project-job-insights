1d2e2831c75fe8bc7c760b1e22991a0038630bb3
tiago-yoneda <xyonedax@gmail.com>
2021-09-17 22:42:17 -0300

    passando requisitos 1 a 9

--- src/insights.py
+++ src/insights.py
@@ -1,150 +1,93 @@
+from src.jobs import read
+
+# from jobs import read
+# import json
+
+path = "src/jobs.csv"
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    job_types = set()
+    full_list = read(path)
+    for job in full_list:
+        job_types.add(job["job_type"])
+    return job_types
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    # jobs é um dic com lista de jobs
+    # job_type é uma string
+    return [job for job in jobs if job["job_type"] == job_type]
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    industry_types = set()
+    full_list = read(path)
+    for job in full_list:
+        industry_type = job["industry"]
+        if industry_type != "":
+            industry_types.add(industry_type)
+    return industry_types
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    # jobs é um dic com lista de jobs
+    # industry é uma string
+    return [job for job in jobs if job["industry"] == industry]
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    full_list = read(path)
+    salary_list = []
+    for job in full_list:
+        if job["max_salary"].isnumeric():
+            salary_list.append(int(job["max_salary"]))
+    salary_list.sort()
+    return salary_list[-1]
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    full_list = read(path)
+    salary_list = []
+    for job in full_list:
+        if job["min_salary"].isnumeric():
+            salary_list.append(int(job["min_salary"]))
+    salary_list.sort()
+    return salary_list[0]
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    # jobs é um dict com min e max salary
+    # erro quando min ou max está ausente
+    # erro quando min ou max não tem valor numerico
+    # jobs é um int
+    if not (
+        "min_salary" in job.keys()
+        and isinstance(job["min_salary"], int)
+        and job["min_salary"] >= 0
+    ):
+        raise ValueError("Problemas com o min_salary")
+    if not (
+        "max_salary" in job.keys()
+        and isinstance(job["max_salary"], int)
+        and job["max_salary"] >= 0
+    ):
+        raise ValueError("Problemas com o max_salary")
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError("O min é maior que o max")
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    job_list = []
+    for job in jobs:
+        if (
+            ("min_salary" in job.keys() and "max_salary" in job.keys())
+            and (job["min_salary"] is not None and job['min_salary' != ''])
+            and (job["max_salary"] is not None and job['max_salary' != ''])
+            and job["min_salary"] <= job["max_salary"]
+            and matches_salary_range(job, salary)
+        ):
+            print(job)
+            job_list.append(job)
+    return job_list
--- src/jobs.py
+++ src/jobs.py
@@ -1,19 +1,9 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    # Boralá
-    return []
+    with open(path, "r") as raw_data:
+        data = csv.DictReader(raw_data)
+        return [job_info for job_info in data]

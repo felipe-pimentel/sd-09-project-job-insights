41c3155112bf94f6ba3eac9695db5e2c14ca1e0b
Henrique Brito Elias <henriquebelias@gmail.com>
2021-09-18 16:52:22 -0300

    Requisitos 8 e 9

--- src/insights.py
+++ src/insights.py
@@ -158,7 +158,17 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    if "min_salary" not in job.keys() or "max_salary" not in job.keys():
+        raise ValueError()
+    if not isinstance(job["min_salary"], int) or not isinstance(
+        job["max_salary"], int
+    ):
+        raise ValueError()
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError()
+    if not isinstance(salary, int):
+        raise ValueError()
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
@@ -176,4 +186,14 @@ def filter_by_salary_range(jobs, salary):
     list
         Jobs whose salary range contains `salary`
     """
-    return []
+    return [
+        job
+        for job in jobs
+        if (
+            isinstance(job["min_salary"], int)
+            and isinstance(job["max_salary"], int)
+            and job["min_salary"] < job["max_salary"]
+            and (isinstance(salary, int))
+            and matches_salary_range(job, salary)
+        )
+    ]

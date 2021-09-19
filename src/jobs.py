5b3a2f74debb12b93fb0c93d5a6f69422dd8a72f
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-09-19 16:27:04 -0300

    req 1 e 2

--- src/jobs.py
+++ src/jobs.py
@@ -1,19 +1,9 @@
-# VQV!
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, mode='r') as file:
+        content = list(csv.DictReader(file))
+        return content

16a5812142019cf8e942b30d1f91fc94138ac50a
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-09-19 18:47:03 -0300

    reqs 8 e 9

--- src/insights.py
+++ src/insights.py
@@ -44,44 +44,25 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    keys = ['min_salary', 'max_salary']
+    if not all(key in job for key in keys):
+        raise ValueError('Missing key')
+    if type(job['min_salary']) is not int or type(job['max_salary']) is not int:
+        raise ValueError('Invalid type')
+    if job['min_salary'] > job['max_salary']:
+        raise ValueError('Min salary is bigger than max salary')
+    if type(salary) is not int:
+        raise ValueError('Invalid salary type')
+    return (job['min_salary'] <= salary <= job['max_salary'])
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    matched_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                matched_jobs.append(job)
+        except ValueError:
+            # print(message)
+            pass
+    return matched_jobs

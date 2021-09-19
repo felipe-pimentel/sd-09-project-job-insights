4b75eb4320fa8c9b29bf6db813e5ab17852a8617
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-09-19 18:51:47 -0300

    fix lint issues

--- src/insights.py
+++ src/insights.py
@@ -5,32 +5,32 @@ def get_unique_job_types(path):
     data = read(path)
     job_types = set()
     for job in data:
-        job_types.add(job['job_type'])
+        job_types.add(job["job_type"])
     return [*job_types]
 
 
 def filter_by_job_type(jobs, job_type):
-    return [job for job in jobs if job['job_type'] == job_type]
+    return [job for job in jobs if job["job_type"] == job_type]
 
 
 def get_unique_industries(path):
     data = read(path)
     industries = set()
     for job in data:
-        industries.add(job['industry'])
-    return [line for line in industries if line != '']
+        industries.add(job["industry"])
+    return [line for line in industries if line != ""]
 
 
 def filter_by_industry(jobs, industry):
-    return [job for job in jobs if job['industry'] == industry]
+    return [job for job in jobs if job["industry"] == industry]
 
 
 def get_max_salary(path):
     data = read(path)
     all_salaries = set()
     for job in data:
-        if job['max_salary'] != '' and job['max_salary'].isdigit():
-            all_salaries.add(int(job['max_salary']))
+        if job["max_salary"] != "" and job["max_salary"].isdigit():
+            all_salaries.add(int(job["max_salary"]))
     return max(all_salaries)
 
 
@@ -38,22 +38,25 @@ def get_min_salary(path):
     data = read(path)
     all_salaries = set()
     for job in data:
-        if job['min_salary'] != '' and job['min_salary'].isdigit():
-            all_salaries.add(int(job['min_salary']))
+        if job["min_salary"] != "" and job["min_salary"].isdigit():
+            all_salaries.add(int(job["min_salary"]))
     return min(all_salaries)
 
 
 def matches_salary_range(job, salary):
-    keys = ['min_salary', 'max_salary']
+    keys = ["min_salary", "max_salary"]
     if not all(key in job for key in keys):
-        raise ValueError('Missing key')
-    if type(job['min_salary']) is not int or type(job['max_salary']) is not int:
-        raise ValueError('Invalid type')
-    if job['min_salary'] > job['max_salary']:
-        raise ValueError('Min salary is bigger than max salary')
+        raise ValueError("Missing key")
+    if (
+        type(job["min_salary"]) is not int
+        or type(job["max_salary"]) is not int
+    ):
+        raise ValueError("Invalid type")
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError("Min salary is bigger than max salary")
     if type(salary) is not int:
-        raise ValueError('Invalid salary type')
-    return (job['min_salary'] <= salary <= job['max_salary'])
+        raise ValueError("Invalid salary type")
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):

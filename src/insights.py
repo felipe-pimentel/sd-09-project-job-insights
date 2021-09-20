a953f0dac53586aafc8c16226f1bbf5ec73d0004
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-09-20 18:42:30 -0300

    get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -16,28 +16,21 @@ def filter_by_job_type(jobs, job_type):
     jobs_filtered = []
 
     for job in jobs:
-        if job['job_type'] == job_type:
+        if job["job_type"] == job_type:
             jobs_filtered.append(job)
 
     return jobs_filtered
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
 
-    Must call `read`
+    industri_types = set()
+    jobs = read(path)
+    for job in jobs:
+        if job["industry"] != "":
+            industri_types.add(job["industry"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industri_types
 
 
 def filter_by_industry(jobs, industry):

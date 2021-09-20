70ee0e0a690f1eea0fdf5bc340e90152f8a0c86b
lcgoandete <lcgoandete@hotmail.com>
2021-09-19 23:00:45 -0300

    requisito 07

--- src/insights.py
+++ src/insights.py
@@ -36,21 +36,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    job_list = []
+    for job in jobs:
+        if (job['industry'] in industry):
+            job_list.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return job_list
 
 
 def get_max_salary(path):

d915aa27c382b33bc48e626a2ffc42efade805ff
Osvaldo Borges <netoo70@gmail.com>
2021-09-20 16:28:22 -0300

    Requisito 08

--- src/insights.py
+++ src/insights.py
@@ -59,28 +59,20 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    if 'max_salary' not in job.keys() or 'min_salary' not in job.keys():
+        raise ValueError("aren't valid integers")
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    max_salary = type(job['max_salary']) is not int
+    min_salary = type(job['max_salary']) is not int
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    if min_salary or max_salary:
+        raise ValueError("aren't valid integers")
+    elif job['min_salary'] > job['max_salary']:
+        raise ValueError("is greather than `job['max_salary']")
+    elif type(salary) is not int:
+        raise ValueError("`salary` isn't a valid integer")
+    return job['min_salary'] <= salary <= job['max_salary']
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
     pass
 
 

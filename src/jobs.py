c35e4a10a5bf01343c0b567241426499fefa11b3
Osvaldo Borges <netoo70@gmail.com>
2021-09-21 18:18:29 -0300

    Requisito 11

--- src/jobs.py
+++ src/jobs.py
@@ -4,21 +4,6 @@ import csv
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-    """
     with open(path, mode='r') as file:
         file_reader = csv.DictReader(file, delimiter=",")
         return [row for row in file_reader]
-    """
-    Parameters
-    ----------
-    path : str
-        Full path to file
-    """
-
-    """
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """

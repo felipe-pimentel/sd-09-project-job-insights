bb5e74a900ded4900597899eb6272068d6c6ec44
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-09-21 20:07:09 -0300

    feat/requirement5

--- src/insights.py
+++ src/insights.py
@@ -56,48 +56,29 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
     jobs_list = read(path)
-    salary = []
+    max_salary = []
     for job in jobs_list:
         if (job["max_salary"] != ''):
             try:
-                salary.append(int(job["max_salary"]))
+                max_salary.append(int(job["max_salary"]))
             except ValueError:
                 print('')
 
-    return int(max(salary))
+    return int(max(max_salary))
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    jobs_list = read(path)
+    min_salary = []
+    for job in jobs_list:
+        if (job["min_salary"] != ''):
+            try:
+                min_salary.append(int(job["min_salary"]))
+            except ValueError:
+                print('')
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return int(min(min_salary))
 
 
 def matches_salary_range(job, salary):

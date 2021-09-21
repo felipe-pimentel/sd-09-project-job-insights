a0b6b1b964cfaf83bd1dc8ee78973f4b6c72aea3
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-09-21 20:21:25 -0300

    feat/requirement6

--- src/insights.py
+++ src/insights.py
@@ -11,21 +11,11 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    jobs_by_type = []
+    for job in jobs:
+        if job_type == job["job_type"]:
+            jobs_by_type.append(job)
+    return jobs_by_type
 
 
 def get_unique_industries(path):

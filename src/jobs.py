1f331de9cb4c681982b25e04a577c169cfa2e507
muriloPereira006 <muriloedu006@gmail.com>
2021-09-21 21:48:27 -0300

    first commit

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,10 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, mode='r') as file:
+        table = csv.reader(file)
+        dict_from_csv = {rows[0]: rows[1] for rows in table}
+        return dict_from_csv

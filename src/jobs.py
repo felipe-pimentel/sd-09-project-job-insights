4d807b518a547f78d20063bd977b87e73ae8eedf
brunobrolesi <brunohsbrolesi@gmail.com>
2021-09-22 16:51:17 -0300

    feat: add req1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,8 @@
 from functools import lru_cache
-
+from csv import DictReader
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, 'r', encoding='utf8') as csvfile:
+        content = DictReader(csvfile)
+        return [rows for rows in content]

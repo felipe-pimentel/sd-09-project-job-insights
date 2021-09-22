f5f5dc1bd3561dde82749f3d3d2ba64479ed1f95
edison-ps <edison@osite.com.br>
2021-09-21 21:35:00 -0300

    Requisito 10 V1.00

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,24 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    data = [
+        {"min_salary": 1000, "max_salary": 3000, "date_posted": "2021-09-11"},
+        {"min_salary": 5000, "max_salary": 7000, "date_posted": "2021-09-01"},
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "2021-09-15"},
+        {"min_salary": 8000, "max_salary": 9000, "date_posted": "2021-09-05"},
+        {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-09-03"},
+        {"min_salary": 4000, "max_salary": 6000, "date_posted": "2021-09-10"}
+    ]
+
+    min_salary = [
+        {"min_salary": 1000, "max_salary": 3000, "date_posted": "2021-09-11"},
+        {"min_salary": 2000, "max_salary": 5000, "date_posted": "2021-09-03"},
+        {"min_salary": 3000, "max_salary": 4000, "date_posted": "2021-09-15"},
+        {"min_salary": 4000, "max_salary": 6000, "date_posted": "2021-09-10"},
+        {"min_salary": 5000, "max_salary": 7000, "date_posted": "2021-09-01"},
+        {"min_salary": 8000, "max_salary": 9000, "date_posted": "2021-09-05"}
+    ]
+
+    sort_by(data, "min_salary")
+    assert data == min_salary

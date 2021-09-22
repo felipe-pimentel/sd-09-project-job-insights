8e23c28cc47fc1cfa38e8b442e8abac53cc6c80b
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-09-22 17:28:31 -0300

    req 8

--- src/insights.py
+++ src/insights.py
@@ -24,21 +24,6 @@ def get_unique_industries(path):
 
 def filter_by_industry(jobs, industry):
     return [row for row in jobs if row["job_type"] == industry]
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
 
 
 def get_max_salary(path):
@@ -62,29 +47,18 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if not ("min_salary" in job.keys() and "max_salary" in job.keys()):
+        raise ValueError("min_salary and max salary must exist")
+    if not (
+        isinstance(job["min_salary"], int)
+        and isinstance(job["max_salary"], int)
+    ):
+        raise ValueError("min_salary and max_salary must be numeric values")
+    if not (isinstance(salary, int)):
+        raise ValueError("salary must be numeric values")
+    if (job["min_salary"] > job["max_salary"]):
+        raise ValueError("max_salary must be bigger than min_salary")
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):

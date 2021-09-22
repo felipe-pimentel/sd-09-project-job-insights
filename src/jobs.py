063727318582984c8f2f73572ffdafd11e7f0901
ggaldino95 <ggaldino95@gmail.com>
2021-09-22 15:07:00 -0300

    [Task 1] read function successfully implemented

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,9 @@
 from functools import lru_cache
+from csv import DictReader
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, 'r', encoding='utf8') as csvfile:
+        spamreader = DictReader(csvfile)
+        return [rows for rows in spamreader]

eb365a2b5d6fe13ccdea9e10b4893cb0db1e3a50
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-09-22 17:14:59 -0300

    Feat: Adicionando as linhas nas colunas de jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,15 +1,16 @@
 import csv
 from functools import lru_cache
-from os import path
 
 
 @lru_cache
 def read(path):
     with open(path) as file:
         jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
-        print('test')
+
         jobs_data = []
         for row in jobs_reader:
             jobs_data.append(row)
 
         return jobs_data
+
+# print(read('src/jobs.csv'))

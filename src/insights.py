e2678c3633c509d237c7e2f1e0be8521727a67e7
Alana Monteiro <lanaah.15@gmail.com>
2021-09-22 16:26:34 -0300

    requisito 8

--- src/insights.py
+++ src/insights.py
@@ -12,21 +12,11 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    jobs_filtered = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            jobs_filtered.append(job)
+    return jobs_filtered
 
 
 def get_unique_industries(path):
@@ -41,21 +31,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    jobs_filtered = []
+    for job in jobs:
+        if job["industry"] == industry:
+            jobs_filtered.append(job)
+    return jobs_filtered
 
 
 def get_max_salary(path):
@@ -72,62 +52,41 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    with open(path) as file:
+        content = csv.DictReader(file)
+        salaries = set()
+        for job in content:
+            if job["min_salary"].isdigit() and job["min_salary"] != "":
+                salaries.add(int(job["min_salary"]))
+    return min(salaries)
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    if (
+        "min_salary" not in job
+        or "max_salary" not in job
+    ):
+        raise ValueError("error")
+    if (
+        type(job["min_salary"]) != int
+        or type(job["max_salary"]) != int
+    ):
+        raise ValueError("error")
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError("error")
+    if type(salary) != int:
+        raise ValueError("error")
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    jobs_filtered = []
+    for job in jobs:
+        if (
+            job["max_salary"] != ""
+            and job["min_salary"] != ""
+        ):
+            if job["min_salary"] <= salary <= job["max_salary"]:
+                jobs_filtered.append(job)
+    return jobs_filtered

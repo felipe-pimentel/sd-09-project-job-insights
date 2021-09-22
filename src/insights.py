460be310d25859a1856e3bdde13bacd880bed5f2
PedroMCHenrique <pedro.876.ph@gmail.com>
2021-09-22 16:31:43 -0300

    req 6

--- src/insights.py
+++ src/insights.py
@@ -10,21 +10,12 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    job_list = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    for job in jobs:
+        if (job["job_type"] == job_type):
+            job_list.append(job)
+    return job_list
 
 
 def get_unique_industries(path):

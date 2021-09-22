ead4015a6c12e3c4421dee60fc0ca3f17437efdc
LucioOSilva <lucioosilva00@gmail.com>
2021-09-22 20:49:41 -0300

    added requirement03

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,15 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    industries_data = read(path)
 
-    Must call `read`
+    industries_list = []
+    for industry in industries_data:
+        if len(industry["industry"]) > 0:
+            industries_list.append(industry["industry"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    industries_list = list(dict.fromkeys(industries_list))
+    return industries_list
 
 
 def filter_by_industry(jobs, industry):

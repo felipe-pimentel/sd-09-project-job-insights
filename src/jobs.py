f637a713e703533c25f82f0b2ab95a2abf879018
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-09-21 22:27:06 -0300

    iniciando o projeto

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,18 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    """Reads a file from a given path and returns its contents"""
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+    with open(path, mode="r") as file:
+        reader = csv.DictReader(file)
+        retorno = [item for item in reader]
+    return retorno
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+
+# algumas referencias
+# https://www.ti-enxame.com/pt/python/criando-um-dicionario-partir-de-um-arquivo-csv/1069729395/
+#  https://www.delftstack.com/pt/howto/python/python-csv-to-dictionary/
+#  https://qastack.com.br/programming/6740918/creating-a-dictionary-from-a-csv-file

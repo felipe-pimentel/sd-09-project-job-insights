a4d13b90d4b05c3392ee0165b275b58db99ff393
muriloPereira006 <muriloedu006@gmail.com>
2021-09-21 21:58:18 -0300

    the return should be an array

--- src/jobs.py
+++ src/jobs.py
@@ -5,6 +5,6 @@ import csv
 @lru_cache
 def read(path):
     with open(path, mode='r') as file:
-        table = csv.reader(file)
-        dict_from_csv = {rows[0]: rows[1] for rows in table}
+        table = csv.DictReader(file)
+        dict_from_csv = [row for row in table]
         return dict_from_csv

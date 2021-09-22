5977e58ffa397b794bddd1fc375050b87c95dfe6
Felipe Cardoso <felipedclc@gmail.com>
2021-09-22 16:36:35 -0300

    Projeto jobs insights, VQV

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,16 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+
+    with open(path, mode="r") as file:
+        jobs_list = csv.DictReader(file)
+        lista = []
+        for value in jobs_list:
+            lista.append(value)
+        return lista
+        # return [value for value in jobs_list]
+
+
+# jobs_list[1:3]
+
+# print(len(read("./jobs.csv")))
---
+++ src/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+with open("./jobs.csv", mode="r") as jobs_file:
+    jobs_list = csv.DictReader(jobs_file)
+    print(jobs_list)

9505487ca0bb53e098b955cfaeb6ef08650b70ab
brenno-calado <brenno.calado@gmail.com>
2021-09-23 15:10:58 -0300

    read jobs.py

--- src/jobs.py
+++ src/jobs.py
@@ -3,16 +3,22 @@ from functools import lru_cache
 
 @lru_cache
 def read(path):
+    import csv
+
     """Reads a file from a given path and returns its contents
 
     Parameters
     ----------
     path : str
         Full path to file
-
+    """
+    with open(path) as file:
+        file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        file_list = [job for job in file_reader]
+    """"
     Returns
     -------
     list
         List of rows as dicts
     """
-    return []
+    return file_list

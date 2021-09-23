d94bcbf53b1a6f397d138e174aad8932735e945b
Felipe <felipe@pop-os.localdomain>
2021-09-23 19:01:03 -0300

    Requirement 5, 6, 7 and 8

--- src/insights.py
+++ src/insights.py
@@ -10,29 +10,56 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    return lambda jobs: jobs.filter(job_type)
+
+    list_jobs = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            list_jobs.append(job)
+    return list_jobs
 
 
 def get_unique_industries(path):
     industry_type = set()
     for industry in read(path):
-        print(industry["industry"])
-        industry_type.add(industry["industry"])
+        if len(industry["industry"]) != 0:
+            industry_type.add(industry["industry"])
     return industry_type
 
 
 def filter_by_industry(jobs, industry):
-    pass
+    jobs_in_industry = []
+    for job in jobs:
+        if job["industry"] == industry:
+            jobs_in_industry.append(job)
+    return jobs_in_industry
 
 
 def get_max_salary(path):
+    jobs = read(path)
 
-    pass
+    max_salary = []
+
+    for job in jobs:
+        try:
+            max_salary.append(int(job["max_salary"]))
+        except ValueError:
+            pass
+    max_salary.sort()
+    return max_salary[-1]
 
 
 def get_min_salary(path):
+    jobs = read(path)
 
-    pass
+    min_salary = []
+
+    for job in jobs:
+        try:
+            min_salary.append(int(job["min_salary"]))
+        except ValueError:
+            pass
+    min_salary.sort()
+    return min_salary[0]
 
 
 def matches_salary_range(job, salary):
@@ -43,3 +70,6 @@ def matches_salary_range(job, salary):
 def filter_by_salary_range(jobs, salary):
 
     return []
+
+
+get_max_salary("src/jobs.csv")

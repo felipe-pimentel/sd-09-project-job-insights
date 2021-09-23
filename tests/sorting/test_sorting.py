0e35038ec614d205317e55ae34a5c191e35d0995
Diogenes Santos <68401286+SantosDiv@users.noreply.github.com>
2021-09-23 00:33:03 -0300

    feat/ Requeremenet 10.1

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,10 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
+from src.jobs import read
 
 
 def test_sort_by_criteria():
+    jobs = read("src/jobs.csv")
+    with pytest.raises(ValueError, match="invalid sorting criteria: invalid"):
+        sort_by(jobs, "invalid")
     pass

10cb34065442c8f3b53ff9446dca3accef4ea12a
Joao Victor <30628255+Galaraz@users.noreply.github.com>
2021-09-23 17:38:15 -0300

    init

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,9 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path) as file:
+        jobs = []
+        read_jobs = csv.DictReader(file, delimiter=",")
+        for job in read_jobs:
+            jobs.append(job)
+    return jobs
--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -1,3 +1,4 @@
+
 from src.jobs import read
 
 

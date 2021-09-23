77fd490e50c8a081287a91bd00a1b635322ed5e3
muriloPereira006 <muriloedu006@gmail.com>
2021-09-22 23:03:32 -0300

    algum ex parou de passar mas n sei qual :/ commitando para ver

--- src/insights.py
+++ src/insights.py
@@ -57,21 +57,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    max_salary = 0
+    jobs_listed = read(path)
+    for job in jobs_listed:
+        if job["max_salary"] != "" and int(job["max_salary"]) > max_salary:
+            max_salary = int(job["max_salary"])
+    return max_salary
 
 
 def get_min_salary(path):

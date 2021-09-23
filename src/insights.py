0cc1eccfdae4c17b9ae5636539fa80ebd1d7ffc9
brenno-calado <brenno.calado@gmail.com>
2021-09-23 18:47:15 -0300

    filter_by_salary_range

--- src/insights.py
+++ src/insights.py
@@ -90,18 +90,13 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    try:
+        filtered_jobs = [
+            job
+            for job in jobs
+            if matches_salary_range(job, salary)
+        ]
+    except ValueError:
+        pass
+    else:
+        return filtered_jobs

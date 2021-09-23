35d86e29b26ce91fc98618b97101602865533cff
Renato Feitosa <pythaqua@gmail.com>
2021-09-23 17:30:47 -0300

    feat(insights): Add 4th and 5th requirements

--- src/insights.py
+++ src/insights.py
@@ -81,6 +81,13 @@ def filter_by_industry(jobs, industry):
     return []
 
 
+def get_max_or_min_salary_from_file(path, get_minimum=False):
+    jobs_data = read(path)
+    property, callback = ("min_salary", min) if get_minimum \
+        else ("max_salary", max)
+    salaries = get_unique_nonempty_entry_property(property, jobs_data)
+    return callback([int(salary) for salary in salaries if salary.isdigit()])
+
 def get_max_salary(path):
     """Get the maximum salary of all jobs
 
@@ -96,7 +103,7 @@ def get_max_salary(path):
     int
         The maximum salary paid out of all job opportunities
     """
-    pass
+    return get_max_or_min_salary_from_file(path, get_minimum=False)
 
 
 def get_min_salary(path):
@@ -114,7 +121,7 @@ def get_min_salary(path):
     int
         The minimum salary paid out of all job opportunities
     """
-    pass
+    return get_max_or_min_salary_from_file(path, get_minimum=True)
 
 
 def matches_salary_range(job, salary):

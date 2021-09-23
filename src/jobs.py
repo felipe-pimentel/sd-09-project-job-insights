73c481254974d1b8b4d46ffdde201249a55ba65d
brenno-calado <brenno.calado@gmail.com>
2021-09-23 15:20:24 -0300

    return list when no error

--- src/jobs.py
+++ src/jobs.py
@@ -4,24 +4,11 @@ from functools import lru_cache
 @lru_cache
 def read(path):
     import csv
-
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-    """
     try:
         with open(path) as file:
             file_reader = csv.DictReader(file, delimiter=",", quotechar='"')
             file_list = [job for job in file_reader]
     except OSError:
         print("Arquivo inexistente")
-    """"
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return file_list
+    else:
+        return file_list

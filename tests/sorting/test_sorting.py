cbda7f313b7822be58590ef48de9e5d660f5796d
Carine Becker <caarine.m@gmail.com>
2021-09-23 19:23:52 -0300

    feat: req10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,24 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def min_salary_mock():
+    return [{
+        "min_salary": "2000"
+    }, {
+        "min_salary": "1000"
+    }]
+
+
+def default_salary_mock():
+    return [{
+        "min_salary": "1000"
+    }, {
+        "min_salary": "2000"
+    }]
+
+
+def test_sorting_by_criteria(min_salary_mock):
+    sort_by(min_salary_mock, "min_salary")
+    assert default_salary_mock() == min_salary_mock

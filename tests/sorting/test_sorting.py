9ae3b8856ed8e183332ea4127dad96058e9d0e21
Christopher <chris.tryber@gmail.com>
2021-09-23 15:03:24 -0300

    start tests

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,15 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {
+            "title": 'Full stack end developer',
+            "min_salary": '4000',
+            "max_salary": '8000'
+        }
+    ]
+    criteria = 'xablau'
+    with pytest.raises(ValueError, match='invalid sorting criteria: xablau'):
+        sort_by(jobs, criteria)

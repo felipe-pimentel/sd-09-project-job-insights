f2f5a17e4bcee4c8b82307f8978521957d3ad47e
Carine Becker <caarine.m@gmail.com>
2021-09-23 19:33:00 -0300

    feat: req10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -11,7 +11,16 @@ def min_salary_mock():
     }]
 
 
-def default_salary_mock():
+@pytest.fixture
+def max_salary_mock():
+    return [{
+        "min_salary": "2000"
+    }, {
+        "min_salary": "1000"
+    }]
+
+
+def min_salary_assert():
     return [{
         "min_salary": "1000"
     }, {
@@ -19,6 +28,17 @@ def default_salary_mock():
     }]
 
 
-def test_sorting_by_criteria(min_salary_mock):
+def max_salary_assert():
+    return [{
+        "min_salary": "2000"
+    }, {
+        "min_salary": "1000"
+    }]
+
+
+def test_sorting_by_criteria(min_salary_mock, max_salary_mock):
     sort_by(min_salary_mock, "min_salary")
-    assert default_salary_mock() == min_salary_mock
+    assert min_salary_assert() == min_salary_mock
+
+    sort_by(max_salary_mock, "max_salary")
+    assert max_salary_assert() == max_salary_mock

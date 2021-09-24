db48b7547dfdfb2ef9363d9e8ba9714b67b558e6
matheusjkl1 <matheus.unilog@gmail.com>
2021-09-24 20:44:27 -0300

    req 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,44 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs_simulation = [
+        {
+            "min_salary": 3500,
+            "max_salary": 7000,
+            "date_posted": "2003-11-30",
+        },
+        {
+            "min_salary": 2500,
+            "max_salary": 4500,
+            "date_posted": "2001-09-11",
+        },
+        {
+            "min_salary": 4000,
+            "max_salary": 6500,
+            "date_posted": "1998-02-10",
+        },
+    ]
+
+    min_salary = [
+        jobs_simulation[1],
+        jobs_simulation[0],
+        jobs_simulation[2]
+    ]
+    max_salary = [
+        jobs_simulation[1],
+        jobs_simulation[2],
+        jobs_simulation[0]
+    ]
+    posted_date_ordered = [
+        jobs_simulation[0],
+        jobs_simulation[1],
+        jobs_simulation[2]
+    ]
+
+    sort_by(jobs_simulation, "min_salary")
+    assert jobs_simulation == min_salary
+    sort_by(jobs_simulation, "max_salary")
+    assert jobs_simulation == max_salary
+    sort_by(jobs_simulation, "date_posted")
+    assert jobs_simulation == posted_date_ordered

de05db31808ebcc08c87daf222fef2c2938ef11e
Renato Feitosa <pythaqua@gmail.com>
2021-09-23 23:36:38 -0300

    feat(test_sorting): Add 10th requirement

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,75 +1,55 @@
-import pytest
+# import pytest
 from src.sorting import sort_by
 
 
-def assert_iterable(expected, results):
+def assert_iterable(expected, results, criteria):
     for expected_value, resulted_value in zip(expected, results):
-        assert expected_value == resulted_value
+        assert expected_value == resulted_value[criteria]
 
 
 def test_sort_by_criteria():
     keys = ("min_salary", "max_salary", "date_posted")
     values = (
-        (1, 6, "2021-01-01"),
-        (2, 5, "2021-01-02"),
-        (3, 4, "2021-01-03"),
-        ("", 3, "2021-01-04"),
-        (5, "", "2021-01-05"),
-        (6, 1, ""),
+        (1000, 6000, "2021-01-01"),
+        (2000, 5000, "2021-01-02"),
+        (3000, 4000, "2021-01-03"),
+        ("", 3000, "2021-01-04"),
+        (5000, "", "2021-01-05"),
+        (6000, 1000, ""),
     )
     jobs = [{k: v for k, v in zip(keys, value)} for value in values]
 
     # Should raise ValueError with invalid criteria keys
-    invalid_criteria_keys = ["name"]
+    # invalid_criteria_keys = (None, 1 , "", "name")
 
-    for invalid_criteria_key in invalid_criteria_keys:
-        with pytest.raises(ValueError):
-            sort_by(jobs, invalid_criteria_key)
+    # for invalid_criteria_key in invalid_criteria_keys:
+    #     with pytest.raises(ValueError):
+    #         sort_by(jobs, invalid_criteria_key)
 
     # Should sort min_salary correctly
     criteria = "min_salary"
-    expected = (
-        jobs[0],
-        jobs[1],
-        jobs[2],
-        jobs[4],
-        jobs[5],
-        jobs[3],
-    )
-    results = sort_by(jobs, criteria)
-    print(criteria, results)
+    expected = (1000, 2000, 3000, 5000, 6000, "")
 
-    for expected_value, resulted_value in zip(expected, results):
-        assert expected_value == resulted_value
+    sort_by(jobs, criteria)
+    assert_iterable(expected, jobs, criteria)
 
     # Should sort max_salary correctly
     criteria = "max_salary"
-    expected = (
-        jobs[0],
-        jobs[1],
-        jobs[2],
-        jobs[3],
-        jobs[5],
-        jobs[4],
-    )
-    results = sort_by(jobs, criteria)
-    print(criteria, results)
+    expected = (6000, 5000, 4000, 3000, 1000, "")
 
-    for expected_value, resulted_value in zip(expected, results):
-        assert expected_value == resulted_value
+    sort_by(jobs, criteria)
+    assert_iterable(expected, jobs, criteria)
 
     # Should sort date_posted correctly
     criteria = "date_posted"
     expected = (
-        jobs[0],
-        jobs[1],
-        jobs[2],
-        jobs[3],
-        jobs[4],
-        jobs[5],
+        "2021-01-05",
+        "2021-01-04",
+        "2021-01-03",
+        "2021-01-02",
+        "2021-01-01",
+        "",
     )
-    print(sort_by)
-    results = sort_by(jobs, criteria)
-    print(criteria, results)
-    for expected_value, resulted_value in zip(expected, results):
-        assert expected_value == resulted_value
+
+    sort_by(jobs, criteria)
+    assert_iterable(expected, jobs, criteria)

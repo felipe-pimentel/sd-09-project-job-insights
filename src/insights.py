fc3c0225cd8675242ed026d3f5a814e42891d42c
cleber-teixeira <cleberlt@gmail.com>
2021-09-24 14:22:58 -0300

    02 - Implementar a função get_unique_job_types. :white_check_mark:

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    unique_job_type = set()
+    for job in read(path):
+        for types in job["job_type"].split(","):
+            unique_job_type.add(types)
+    return len(unique_job_type)
 
 
 def filter_by_job_type(jobs, job_type):

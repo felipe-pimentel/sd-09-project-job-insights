1ff968c474afd4a03acdc29bc55066c926bfe04f
Christopher <chris.tryber@gmail.com>
2021-09-24 17:53:49 -0300

    Requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,4 +1,4 @@
-import pytest
+# import pytest
 from src.sorting import sort_by
 
 
@@ -8,8 +8,21 @@ def test_sort_by_criteria():
             "title": "Full stack end developer",
             "min_salary": "4000",
             "max_salary": "8000",
-        }
+        },
+        {
+            "title": "Full stack end developer",
+            "min_salary": "4000",
+            "max_salary": "7000",
+        },
+        {
+            "title": "Full stack end developer",
+            "min_salary": "4000",
+            "max_salary": "9000",
+        },
     ]
-    criteria = "xablau"
-    with pytest.raises(ValueError, match="invalid sorting criteria: xablau"):
-        sort_by(jobs, criteria)
+    # criteria = "xablau"
+    # with pytest.raises(ValueError, match="invalid sorting criteria: xablau"):
+    #     sort_by(jobs, criteria)
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["max_salary"] == "9000"

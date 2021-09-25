ed761ed56091d7738c5235c03d9a610cdb8e7a91
joao <joaocarlitoneto@gmail.com>
2021-09-24 21:17:54 -0300

    Req 09 Ok

--- src/insights.py
+++ src/insights.py
@@ -202,4 +202,20 @@ def filter_by_salary_range(jobs, salary):
     list
         Jobs whose salary range contains `salary`
     """
-    return []
+    #  https://www.programiz.com/python-programming/methods/dictionary/keys
+    filter_salary_range = []
+
+    for job in jobs:
+        if (
+            isinstance(salary, int)
+            and ("min_salary" in job.keys() and "max_salary" in job.keys())
+            and isinstance(job["max_salary"], int)
+            and isinstance(job["min_salary"], int)
+            and int(job["min_salary"]) >= 0 and int(job["max_salary"]) >= 0
+            and int(job["min_salary"]) <= int(job["max_salary"])
+            and matches_salary_range(job, salary)
+        ):
+            filter_salary_range.append(job)
+            #  método anexa um elemento ao final da lista
+
+    return filter_salary_range

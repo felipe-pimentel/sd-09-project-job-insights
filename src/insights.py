d79394506de7f401e3b1d1ed5f1fa192035e14d8
Douglas Ferreira <dodofp.df@gmail.com>
2021-09-26 16:55:50 -0300

    Requisito 6

--- src/insights.py
+++ src/insights.py
@@ -15,21 +15,13 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    jobs_to_return = []
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    for job in jobs:
+        if job_type == job['job_type']:
+            jobs_to_return.append(job)
+    return jobs_to_return
 
 
 def get_unique_industries(path):

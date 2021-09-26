6280a97ccb2ceab5436261b51e585b1e03ea4910
jhon <jhonatan.arguello@gmail.com>
2021-09-25 23:13:04 -0300

    Finalizaçao do requisito 7.

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,13 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    jobs_list = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    for job in jobs:
+        if job["industry"] == industry:
+            jobs_list.append(job)
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return jobs_list
 
 
 def get_max_salary(path):

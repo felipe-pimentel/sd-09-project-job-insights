1f2cfa35931aa1d99688df90073e1237cc976417
Douglas Ferreira <dodofp.df@gmail.com>
2021-09-26 17:35:38 -0300

    Requisito 8

--- src/insights.py
+++ src/insights.py
@@ -75,42 +75,17 @@ def get_min_salary(path):
 
 def matches_salary_range(job, salary):
     # https://www.geeksforgeeks.org/python-get-dictionary-keys-as-a-list/
-    if  'min_salary' not in job.keys() or 'max_salary' not in job.keys():
+    if 'min_salary' not in job.keys() or 'max_salary' not in job.keys():
         raise ValueError("min_salary or max_salary doens't exist")
-    if  type(job['min_salary']) != int or type(job['max_salary']) != int:
+    if type(job['min_salary']) != int or type(
+            job['max_salary']) != int or type(salary) != int:
         raise ValueError("min_salary and max_salary must be numbers")
     if job['min_salary'] > job['max_salary']:
         raise ValueError("min_salary can't be higher than max_salary")
-    if type(salary) != int:
-        raise ValueError("salary must be a number")
     if job['min_salary'] <= salary <= job['max_salary']:
         return True
-    else: return False
-
-
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    else:
+        return False
 
 
 def filter_by_salary_range(jobs, salary):

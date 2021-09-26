7d2bd9a8505e2c243c7fe9c33f0edcaa3bfe02b7
Uilton <itoclick@gmail.com>
2021-09-26 01:13:02 -0300

    jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,22 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
 
+    with open("jobs.csv", "w") as file:
+        header_table = csv.reader(file, delimiter=",", quotechar='"')
+        header, *table = header_table
+
+        """Reads a file from a given path and returns its contents
     Parameters
     ----------
     path : str
         Full path to file
-
     Returns
     -------
     list
         List of rows as dicts
     """
-    return []
+    return [table]

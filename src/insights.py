61957ef3158b052a2409e7fc203b288a6cace09c
cleber-teixeira <cleberlt@gmail.com>
2021-09-27 15:34:31 -0300

    03 - Implementar a função "get_unique_industries". :white_check_mark:

--- src/insights.py
+++ src/insights.py
@@ -56,7 +56,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    unique_industries = set()
+    for ind in read(path):
+        if ind["industry"]:
+            unique_industries.add(ind["industry"])
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):

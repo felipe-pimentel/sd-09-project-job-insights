01977550b114aa3b9164c2198ddcbb3ea3577249
Saulo Kirchmaier <saulokirchmaier@gmail.com>
2021-09-27 20:05:04 -0300

    Req. 01 ao 10

--- src/jobs.py
+++ src/jobs.py
@@ -5,5 +5,7 @@ import csv
 @lru_cache
 def read(path):
     with open(path) as file_csv:
-        file_reader = list(csv.DictReader(file_csv, delimiter=",", quotechar='"'))
+        file_reader = list(
+            csv.DictReader(file_csv, delimiter=",", quotechar='"')
+        )
     return file_reader

fc63084ee3d2ca4ac1329cbca7619191a8e94632
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-09-27 17:26:59 -0300

    funcao filter_by_industry, cria uma lista com as industrias especificadas

--- src/insights.py
+++ src/insights.py
@@ -34,21 +34,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    industry_list = []
+    for job in jobs:
+        if job["industry"] == industry:
+            industry_list.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return industry_list
 
 
 def get_max_salary(path):

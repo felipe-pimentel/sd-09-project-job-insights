7df0369c3e3b494fc5c2d0c2b07019b840dd439b
brenno-calado <brenno.calado@gmail.com>
2021-09-27 19:35:01 -0300

    test_sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,15 +1,45 @@
-import pytest
 from src.sorting import sort_by
 
 
 def test_sorting_by_criteria():
     jobs = [
-        {"max_salary": 0, "min_salary": 10, "date_posted": "2020-05-08"},
-        {"max_salary": 10, "min_salary": 100, "date_posted": "2020-05-08"},
-        {"max_salary": 10000, "min_salary": 200, "date_posted": "2020-06-07"},
-        {"max_salary": 15000, "min_salary": 0, "date_posted": "2020-04-28"},
-        {"max_salary": 1500, "min_salary": 0, "date_posted": "2020-02-28"},
-        {"max_salary": -1, "min_salary": 10, "date_posted": "2020-04-20"},
+        {
+            "max_salary": "1000",
+            "min_salary": "10",
+            "date_posted": "2020-05-08"
+        },
+        {
+            "max_salary": "1000",
+            "min_salary": "100",
+            "date_posted": "2020-05-08"
+        },
+        {
+            "max_salary": "10000",
+            "min_salary": "200",
+            "date_posted": "2020-06-07"
+        },
+        {
+            "max_salary": "15000",
+            "min_salary": "0",
+            "date_posted": "2020-04-28"
+        },
+        {
+            "max_salary": "1500",
+            "min_salary": "5",
+            "date_posted": "2020-02-28"
+        },
+        {
+            "max_salary": "1200",
+            "min_salary": "10",
+            "date_posted": "2020-04-20"
+        },
     ]
-    with pytest.raises(ValueError, match="invalid sorting criteria: random"):
-        sort_by(jobs, "random")
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["max_salary"] == "15000"
+
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["min_salary"] == "0"
+
+    sort_by(jobs, "date_posted")
+    assert jobs[0]["date_posted"] == "2020-06-07"

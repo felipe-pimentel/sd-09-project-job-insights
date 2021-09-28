ad1142b00b14a3b26f163dc3e15e7d006211485e
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-09-28 11:23:07 -0300

    teste sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,29 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    JOBS = [
+        {
+            "min_salary": "1000",
+            "max_salary": "8000",
+            "date_posted": "2020-08-28",
+        },
+        {
+            "min_salary": "2000",
+            "max_salary": "7000",
+            "date_posted": "2021-08-28",
+        },
+        {
+            "min_salary": "7777",
+            "max_salary": "14444",
+            "date_posted": "2020-12-25",
+        },
+    ]
+    sort_by(JOBS, "max_salary")
+    assert JOBS[0]["max_salary"] == "14444"
+
+    sort_by(JOBS, "min_salary")
+    assert JOBS[0]["min_salary"] == "1000"
+
+    sort_by(JOBS, "date_posted")
+    assert JOBS[0]["date_posted"] == "2021-08-28"

51dea9b58714f4beaeccb7348c4873bbce10659c
Alvaro Braga <alvaro.braga.job@gmail.com>
2021-09-28 14:05:59 -0300

    Req 09

--- src/insights.py
+++ src/insights.py
@@ -71,18 +71,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    valid_jobs = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                valid_jobs.append(job)
+        except ValueError:
+            pass
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    return valid_jobs

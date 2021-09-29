346e4415213115bda2dbec73f0c0a804fc925075
Alana Monteiro <lanaah.15@gmail.com>
2021-09-29 15:46:04 -0300

    teste 1

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,37 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"id": 1,
+         "min_salary": 500,
+         "max_salary": 1000,
+         "date_posted": "2020-05-07"},
+        {"id": 2,
+         "min_salary": 300,
+         "max_salary": 3000,
+         "date_posted": "2020-05-08"},
+        {"id": 3,
+         "min_salary": 100,
+         "max_salary": 150,
+         "date_posted": "2020-05-06"},
+    ]
+
+    sorted_jobs_min_salary = [
+            {"id": 3,
+             "min_salary": 100,
+             "max_salary": 150,
+             "date_posted": "2020-05-06"},
+            {"id": 2,
+             "min_salary": 300,
+             "max_salary": 3000,
+             "date_posted": "2020-05-08"},
+            {"id": 1,
+             "min_salary": 500,
+             "max_salary": 1000,
+             "date_posted": "2020-05-07"},
+        ]
+
+    sort_by(jobs, "min_salary")
+
+    assert jobs == sorted_jobs_min_salary

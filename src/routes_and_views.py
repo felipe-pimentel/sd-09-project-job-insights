873c0a6d95893c4d1671428cff68e36fb2594324
Luciano_Lodi <lrlodi@gmail.com>
2021-09-30 15:29:12 -0300

    Requisito 11

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -11,7 +11,7 @@ from .insights import (
     get_min_salary,
     get_max_salary,
 )
-from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls
+from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls, get_job
 
 bp = Blueprint("client", __name__, template_folder="templates")
 
@@ -57,6 +57,14 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+
+@bp.route("/job/<index>")
+def job(index):
+    jobs = read("src/jobs.csv")
+    target_job = get_job(jobs, index)
+
+    return render_template("job.jinja2", job=target_job)
 
 
 def init_app(app: Flask):

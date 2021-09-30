ff64132af262a5de32ef5e2b7a3df11961d1a30c
Bidfca <bruninhakjurugat11@gmail.com>
2021-09-30 01:37:29 -0300

    1-10

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,9 @@
 from functools import lru_cache
-
+from csv import DictReader
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+    with open(path) as file:
+        return list(DictReader(file))
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []

e0e9eea00cb43c89deee299b0fbf6871b13692e8
Emanoel Leão <Emanoel Leo>
2021-10-01 02:08:28 -0300

    Requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,56 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+jobs = [
+    {
+        'min_salary': 2000,
+        'max_salary': 5000,
+    },
+    {
+        'min_salary': 0,
+        'max_salary': 1000,
+    },
+    {
+        'min_salary': 6000,
+        'max_salary': 10000,
+    }
+]
+
+jobs_order_by_min_salary = [
+    {
+        'min_salary': 0,
+        'max_salary': 1000,
+    },
+    {
+        'min_salary': 2000,
+        'max_salary': 5000,
+    },
+    {
+        'min_salary': 6000,
+        'max_salary': 10000,
+    }
+]
+
+jobs_order_by_max_salary = [
+    {
+        'min_salary': 6000,
+        'max_salary': 10000,
+    },
+    {
+        'min_salary': 2000,
+        'max_salary': 5000,
+    },
+    {
+        'min_salary': 0,
+        'max_salary': 1000,
+    },
+]
 
 
 def test_sort_by_criteria():
+    sort_by(jobs, 'min_salary')
+    assert jobs == jobs_order_by_min_salary
+
+    sort_by(jobs, 'max_salary')
+    assert jobs == jobs_order_by_max_salary
     pass

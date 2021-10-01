c1638ddcac4c098024869c6eeda82158f12bc8de
Emanoel Leão <Emanoel Leo>
2021-10-01 00:37:13 -0300

    Requisito 04'

--- src/insights.py
+++ src/insights.py
@@ -58,21 +58,14 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    salaries = []
+    jobs = read(path)
 
-    Must call `read`
+    for job in jobs:
+        if job['max_salary'] != '' and job['max_salary'] != 'invalid':
+            salaries.append(int(job['max_salary']))
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    return max(salaries)
 
 
 def get_min_salary(path):

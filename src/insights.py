c13614b31e00a6d636dfeb727d54815d49b9aef6
ViniciusChagas <viniciushrcs@gmail.com>
2021-10-02 09:41:11 -0300

    Requisitos 4 e 5

--- src/insights.py
+++ src/insights.py
@@ -56,39 +56,21 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = src.jobs.read(path)
+    data_jobs_max_salary = set()
+    for job in jobs_list:
+        if job["max_salary"] != "":
+            data_jobs_max_salary.add(job["max_salary"])
+    return max(data_jobs_max_salary)
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = src.jobs.read(path)
+    data_jobs_min_salary = set()
+    for job in jobs_list:
+        if job["min_salary"] != "":
+            data_jobs_min_salary.add(job["min_salary"])
+    return min(data_jobs_min_salary)
 
 
 def matches_salary_range(job, salary):

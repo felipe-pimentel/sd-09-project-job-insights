55e2228e353f7cbe0af6d970bb4c6dbae3421dbd
Lucas Neiva <lucassneiva.code@gmail.com>
2021-10-02 20:29:14 -0300

    fix test

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -24,16 +24,16 @@ def db_example():
 
 
 def test_sort_by_criteria(db_example):
+    sort_by(db_example, "max_salary")
+    assert [x["max_salary"] for x in db_example] == [150000, 80000, 60000]
+
+    sort_by(db_example, "min_salary")
+    assert [x["min_salary"] for x in db_example] == [5000, 6000, 7000]
 
     sort_by(db_example, "date_posted")
     assert [x["date_posted"] for x in db_example] == [
-        "2021-10-18",
         "2021-11-25",
-        "2021-01-30"
+        "2021-10-18",
+        "2021-01-30",
         ]
 
-    sort_by(db_example, "max_salary")
-    assert [x["max_salary"] for x in db_example] == [60000, 150000, 80000]
-
-    sort_by(db_example, "min_salary")
-    assert [x["min_salary"] for x in db_example] == [6000, 5000, 7000]

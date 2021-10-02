c242d4a015fd0aea9a1ac52ea30f9e26c30e6c42
Leticia Lima <letylima96@gmail.com>
2021-10-02 18:56:52 -0300

    requisito 7

--- src/insights.py
+++ src/insights.py
@@ -27,21 +27,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filteredjobs = []
+    for eachjob in jobs:
+        if (eachjob["industry"] == industry):
+            filteredjobs.append(eachjob)
+    return filteredjobs
 
 
 def get_max_salary(path):

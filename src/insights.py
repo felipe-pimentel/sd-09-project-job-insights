bb5ca614597ef8563fd89f7a8b3dea7a2695c004
Leticia Lima <letylima96@gmail.com>
2021-10-02 18:41:01 -0300

    requisito 3

--- src/insights.py
+++ src/insights.py
@@ -28,21 +28,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    jobsfile = read(path)
+    industries = set()
+    for eachjob in jobsfile:
+        if eachjob["industry"] != "":
+            industries.add(eachjob["industry"])
+    return industries
 
 
 def filter_by_industry(jobs, industry):

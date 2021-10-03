c4cabfbb16ec0407e41c348b67a3f8ee8b59a327
Leticia Lima <letylima96@gmail.com>
2021-10-03 19:28:10 -0300

    requisito 2 a 8

--- src/insights.py
+++ src/insights.py
@@ -1,109 +1,57 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    jobsfile = read(path)
+    job_types = set()
+    for eachjob in jobsfile:
+        job_types.add(eachjob["job_type"])
+    return job_types
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    filteredjobs = []
+    for eachjob in jobs:
+        if (eachjob["job_type"] == job_type):
+            filteredjobs.append(eachjob)
+    return filteredjobs
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    jobsfile = read(path)
+    industries = set()
+    for eachjob in jobsfile:
+        if (eachjob["industry"] != ""):
+            industries.add(eachjob["industry"])
+    return industries
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filteredjobs = []
+    for eachjob in jobs:
+        if (eachjob["industry"] == industry):
+            filteredjobs.append(eachjob)
+    return filteredjobs
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobsfile = read(path)
+    salaries = set()
+    for eachjob in jobsfile:
+        if (eachjob["max_salary"].isnumeric()):
+            salaries.add(int(eachjob["max_salary"]))
+    max_salary = max(salaries, key=int)
+    return max_salary
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    jobsfile = read(path)
+    salaries = set()
+    for eachjob in jobsfile:
+        if (eachjob["min_salary"].isnumeric()):
+            salaries.add(int(eachjob["min_salary"]))
+    min_salary = min(salaries, key=int)
+    return min_salary
 
 
 def matches_salary_range(job, salary):

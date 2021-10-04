b0703f363e2a2767d628e7c0943bf51695d2d801
Julianafatsil <julianafatsil@gmail.com>
2021-10-04 20:44:40 -0300

    Create function get_max_salary with isnumeric validate and max!

--- src/insights.py
+++ src/insights.py
@@ -57,21 +57,15 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    jobs_list = read(path)
+    max_salary = [
+        int(job["max_salary"])
+        for job in jobs_list if job["max_salary"].isnumeric()
+    ]
+    return max(max_salary)
 
-    Must call `read`
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+# print(get_max_salary("src/jobs.csv"))
 
 
 def get_min_salary(path):

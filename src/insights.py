1b1e354f8b53e0742392f8e414b7ac9047eb3904
Julianafatsil <julianafatsil@gmail.com>
2021-10-04 19:25:23 -0300

    Create requeriment2 with list and set

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,13 @@
+from jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
+    jobs = read(path)
+    type_job = [job["job_type"] for job in jobs]  # read de traz pra fente
+    return list(set(type_job))
 
-    Must call `read`
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+#  print(get_unique_job_types("src/jobs.csv"))
 
 
 def filter_by_job_type(jobs, job_type):

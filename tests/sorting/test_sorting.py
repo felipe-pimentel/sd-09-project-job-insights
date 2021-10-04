b59bf6968b5b82db17fada4b9c8d0d6511a63a17
Enio Nicael <enionicael@gmail.com>
2021-10-04 17:15:29 -0300

    Refac: refactoring the test_sort_by_criteria test

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -3,19 +3,86 @@ from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-
-    # criteria_types = ['min_salary', 'max_salary', 'date_posted']
-
     jobs = [
-        {"title": "Front end developer", "salary": "2000", "type": "trainee"},
-        {"title": "Back end developer", "salary": "3000", "type": "full time"},
         {
-            "title": "Full stack end developer",
-            "salary": "4000",
-            "type": "full time",
+            "min_salary": "5000",
+            "max_salary": "6279",
+            "date_posted": "2020-06-07"
+        },
+        {
+            "min_salary": "2000",
+            "max_salary": "3279",
+            "date_posted": "2020-01-07"
+        },
+        {
+            "min_salary": "7000",
+            "max_salary": "8279",
+            "date_posted": "2020-03-07"
         },
     ]
 
-    # reject unspecified criteria
+    min_salary_order = [
+        {
+            "min_salary": "2000",
+            "max_salary": "3279",
+            "date_posted": "2020-01-07"
+        },
+        {
+            "min_salary": "5000",
+            "max_salary": "6279",
+            "date_posted": "2020-06-07"
+        },
+        {
+            "min_salary": "7000",
+            "max_salary": "8279",
+            "date_posted": "2020-03-07"
+        },
+    ]
+
+    max_salary_order = [
+        {
+            "min_salary": "7000",
+            "max_salary": "8279",
+            "date_posted": "2020-03-07"
+        },
+        {
+            "min_salary": "5000",
+            "max_salary": "6279",
+            "date_posted": "2020-06-07"
+        },
+        {
+            "min_salary": "2000",
+            "max_salary": "3279",
+            "date_posted": "2020-01-07"
+        }
+    ]
+
+    date_posted_order = [
+        {
+            "min_salary": "5000",
+            "max_salary": "6279",
+            "date_posted": "2020-06-07"
+        },
+        {
+            "min_salary": "7000",
+            "max_salary": "8279",
+            "date_posted": "2020-03-07"
+        },
+        {
+            "min_salary": "2000",
+            "max_salary": "3279",
+            "date_posted": "2020-01-07"
+        }
+    ]
+
+    sort_by(jobs, "min_salary")
+    assert jobs == min_salary_order
+
+    sort_by(jobs, "max_salary")
+    assert jobs == max_salary_order
+
+    sort_by(jobs, "date_posted")
+    assert jobs == date_posted_order
+
     with pytest.raises(ValueError):
         sort_by(jobs, "title")

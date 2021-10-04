7531d45e4aedb7f569e3824f46afa9554f0e52ea
Leticia Lima <letylima96@gmail.com>
2021-10-04 20:00:25 -0300

    requisito 9

--- src/insights.py
+++ src/insights.py
@@ -74,18 +74,11 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    filteredjobs = []
+    for eachjob in jobs:
+        try:
+            if (matches_salary_range(eachjob, salary)):
+                filteredjobs.append(eachjob)
+        except Exception:
+            print('Erro')
+    return filteredjobs

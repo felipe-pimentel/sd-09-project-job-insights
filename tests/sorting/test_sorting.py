826aa0d37bb4f56495b96ee98b47117943573802
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-10-04 09:27:33 -0300

    req10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,32 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs_list = [
+        {
+            "max_salary": "2000",
+            "min_salary": "1000",
+            "date_posted": "2021-03-01",
+        },
+        {
+            "max_salary": "4000",
+            "min_salary": "2001",
+            "date_posted": "2021-04-01",
+        },
+        {
+            "max_salary": "6000",
+            "min_salary": "4001",
+            "date_posted": "2021-05-01"
+        }
+    ]
+
+    criteria = ['min_salary', 'max_salary', 'date_posted']
+
+    sort_by(jobs_list, criteria[0])
+    assert jobs_list[0]["min_salary"] == "1000"
+
+    sort_by(jobs_list, criteria[1])
+    assert jobs_list[0]["max_salary"] == "2000"
+
+    sort_by(jobs_list, criteria[2])
+    assert jobs_list[0]["date_posted"] == "2021-03-01"

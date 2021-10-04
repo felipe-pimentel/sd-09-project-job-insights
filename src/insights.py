66153dc2ac843d9a1877dc009173dab141ea25e1
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-10-04 19:28:48 -0300

    feat: add requirement 2

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,11 @@
+from src.jobs import read
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    jobs = read(path)
+    typesJobs = set()
+    for job in jobs:
+        typesJobs.add(job["job_type"])
+    return typesJobs
 
 
 def filter_by_job_type(jobs, job_type):

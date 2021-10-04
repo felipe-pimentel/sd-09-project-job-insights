9819b85c9aa7b2ecbcae0c65f336eb87f5dede03
Nathália Veneziano <nathaliaveneziano@icloud.com>
2021-10-04 18:07:44 -0300

    Fix - Requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -5,39 +5,40 @@ from src.sorting import sort_by
 @pytest.fixture
 def sorting_by_criteria():
     return [
-        {"max_salary": 726, "min_salary": 0, "date_posted": "2011-01-01"},
+        {"max_salary": 726, "min_salary": 2, "date_posted": "2011-01-01"},
         {"max_salary": 479, "min_salary": 23, "date_posted": "2014-02-28"},
         {"max_salary": 15848, "min_salary": 546, "date_posted": "2018-12-17"},
         {"max_salary": 15000, "min_salary": 5, "date_posted": "2012-08-04"},
         {"max_salary": 8723, "min_salary": 1, "date_posted": "2020-01-13"},
         {"max_salary": 33682, "min_salary": 2639, "date_posted": "2011-02-16"},
+        {"max_salary": None, "min_salary": None, "date_posted": None},
     ]
 
 
 def test_sort_by_criteria(sorting_by_criteria):
-    sort_types = ["min_salary", "max_salary", "date_posted"]
+    sort_types = ['max_salary', 'min_salary', 'date_posted']
+    expect_min_salary = [1, 2, 5, 23, 546, 2639]
+    expect_max_salary = [33682, 15848, 15000, 8723, 726, 479]
+    expect_date_posted = [
+        "2020-01-13",
+        "2018-12-17",
+        "2014-02-28",
+        "2012-08-04",
+        "2011-02-16",
+        "2011-01-01",
+    ]
 
     for type in sort_types:
-        result = sort_by(sorting_by_criteria, type)
+        list_sorting = sort_by(sorting_by_criteria, type)
 
-        if type == "min_salary":
-            assert result[0]["min_salary"] == 0
-            assert result[1]["min_salary"] == 1
-            assert result[2]["min_salary"] == 5
-            assert result[3]["min_salary"] == 23
-            assert result[4]["min_salary"] == 546
-            assert result[5]["min_salary"] == 2639
-        elif type == "max_salary":
-            assert result[0]["max_salary"] == 33682
-            assert result[1]["max_salary"] == 15848
-            assert result[2]["max_salary"] == 15000
-            assert result[3]["max_salary"] == 8723
-            assert result[4]["max_salary"] == 726
-            assert result[5]["max_salary"] == 479
+        if list_sorting is None:
+            assert list_sorting is None
         else:
-            assert result[0]["date_posted"] == "2020-01-13"
-            assert result[1]["date_posted"] == "2018-12-17"
-            assert result[2]["date_posted"] == "2014-02-28"
-            assert result[3]["date_posted"] == "2012-08-04"
-            assert result[4]["date_posted"] == "2011-02-16"
-            assert result[5]["date_posted"] == "2011-01-01"
+            list_sorting = [job[type] for job in list_sorting]
+
+            if type == "min_salary":
+                assert list_sorting == expect_min_salary
+            elif type == "max_salary":
+                assert list_sorting == expect_max_salary
+            else:
+                assert list_sorting == expect_date_posted

d2b3da8b661bd40ef57f21e4d2e4f1aef3db59ed
Mario Pardo <mariio.pardojr@gmail.com>
2021-10-04 15:12:23 -0300

    Requisito 4 e 5

--- src/insights.py
+++ src/insights.py
@@ -55,39 +55,21 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs = read(path)
+    max_salary_list = []
+    for job in jobs:
+        if job["max_salary"].isdigit():
+            max_salary_list.append(int(job["max_salary"]))
+    return max(max_salary_list)
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    jobs = read(path)
+    min_salary_list = []
+    for job in jobs:
+        if job["min_salary"].isdigit():
+            min_salary_list.append(int(job["min_salary"]))
+    return min(min_salary_list)
 
 
 def matches_salary_range(job, salary):

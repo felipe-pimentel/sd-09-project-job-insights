00173a31af5c145cdeea091172812a1b14cbb312
Mario Pardo <mariio.pardojr@gmail.com>
2021-10-05 00:08:09 -0300

    Resquito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,35 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"min_salary": 10, "max_salary": 30, "date_posted": "05-12-2019"},
+        {"min_salary": 60, "max_salary": 90, "date_posted": "04-13-2019"},
+        {"min_salary": 20, "max_salary": 50, "date_posted": "12-01-2019"},
+    ]
+
+    ordered_by_max_salary = [
+        {"min_salary": 60, "max_salary": 90, "date_posted": "04-13-2019"},
+        {"min_salary": 20, "max_salary": 50, "date_posted": "12-01-2019"},
+        {"min_salary": 10, "max_salary": 30, "date_posted": "05-12-2019"},
+    ]
+
+    ordered_by_min_salary = [
+        {"min_salary": 10, "max_salary": 30, "date_posted": "05-12-2019"},
+        {"min_salary": 20, "max_salary": 50, "date_posted": "12-01-2019"},
+        {"min_salary": 60, "max_salary": 90, "date_posted": "04-13-2019"},
+    ]
+
+    sort_by(jobs, "max_salary")
+
+    assert jobs == ordered_by_max_salary
+
+    sort_by(jobs, "min_salary")
+
+    assert jobs == ordered_by_min_salary
+
+    with pytest.raises(
+        ValueError, match="invalid sorting criteria: date-posted"
+    ):
+        sort_by(jobs, "date-posted")

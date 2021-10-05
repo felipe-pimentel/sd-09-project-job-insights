e0597938a9706c8d79d6c92a3e6c9132c0fffbab
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-10-05 03:38:13 -0300

    Req 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,33 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
+
+jobs = [
+    {
+        "min_salary": 1000,
+        "max_salary": 2000,
+        "date_posted": "2021-07-19"
+    },
+    {
+        "min_salary": 3000,
+        "max_salary": 4000,
+        "date_posted": "2021-01-01"
+    },
+    {
+        "min_salary": 5000,
+        "max_salary": 6000,
+        "date_posted": "2021-01-02"
+    },
+]
 
 def test_sort_by_criteria():
-    pass
+    criteria = "min_salary"
+    sort_by(jobs,criteria)
+    assert jobs[0]["min_salary"] == 1000
+
+    criteria = "max_salary"
+    sort_by(jobs,criteria)
+    assert jobs[0]["max_salary"] == 6000
+
+    criteria = "date_posted"
+    sort_by(jobs,criteria)
+    assert jobs[0]["min_salary"] == "2021-07-19"

681aecf0e144d0ddd94c4db885b8504f85622334
Daniel Cespedes <dev.danielcespedes@gmail.com>
2021-10-04 23:27:35 -0300

    put test sorting in the right place

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,24 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs_list = [
+    {"min_salary": 1, "max_salary": 6, "date_posted": "05-11-2010"},
+    {"min_salary": 2, "max_salary": 7, "date_posted": "08-11-2010"},
+    {"min_salary": 3, "max_salary": 8, "date_posted": "06-11-2010"},
+    {"min_salary": 4, "max_salary": 9, "date_posted": "04-11-2010"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs_list, "max_salary")
+    assert [job["max_salary"] for job in jobs_list] == [9, 8, 7, 6]
+
+    sort_by(jobs_list, "min_salary")
+    assert [job["min_salary"] for job in jobs_list] == [1, 2, 3, 4]
+
+    sort_by(jobs_list, "date_posted")
+    assert [job["date_posted"] for job in jobs_list] == [
+        "05-11-2010",
+        "08-11-2010",
+        "06-11-2010",
+        "04-11-2010",
+      ]

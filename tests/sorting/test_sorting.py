ea34aa2cc01c3f3b9b9c0304acf5bacec3d8283e
Felipe Rocha <felipe.sr27@gmail.com>
2021-10-05 01:28:49 -0300

    Order By Tests

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,63 @@
-# from src.sorting import sort_by
-
+from src.sorting import sort_by
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {
+            "title": "Frontend dev",
+            "min_salary": "2000",
+            "max_salary": "6000",
+            "date_posted": "2020-04-12",
+        },
+        {
+            "title": "Fullstack dev",
+            "min_salary": "4000",
+            "max_salary": "8000",
+            "date_posted": "2020-01-01",
+        },
+        {
+            "title": "Backend dev",
+            "min_salary": "3000",
+            "max_salary": "7000",
+            "date_posted": "2021-04-03",
+        },
+    ]
+    assert sort_by(jobs, 'min_salary') == [
+        {
+            "title": "Frontend dev",
+            "min_salary": "2000",
+            "max_salary": "6000",
+            "date_posted": "2020-04-12",
+        },
+        {
+            "title": "Backend dev",
+            "min_salary": "3000",
+            "max_salary": "7000",
+            "date_posted": "2021-04-03",
+        },
+        {
+            "title": "Fullstack dev",
+            "min_salary": "4000",
+            "max_salary": "8000",
+            "date_posted": "2020-01-01",
+        },
+    ]
+    assert sort_by(jobs, 'date_posted') == [
+        {
+            "title": "Frontend dev",
+            "min_salary": "2000",
+            "max_salary": "6000",
+            "date_posted": "2020-04-12",
+        },
+        {
+            "title": "Backend dev",
+            "min_salary": "3000",
+            "max_salary": "7000",
+            "date_posted": "2021-04-03",
+        },
+        {
+            "title": "Fullstack dev",
+            "min_salary": "4000",
+            "max_salary": "8000",
+            "date_posted": "2020-01-01",
+        },
+    ]

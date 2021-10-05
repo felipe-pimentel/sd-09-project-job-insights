28631c8b80022f708235fd4a351ac850f0bd9827
marcusWittho <mcswitthoeft@gmail.com>
2021-10-05 00:15:21 -0300

    Requisito 10 - Implementação dos testes de ordenação

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,81 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+all_jobs = [
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "04-10-2021",
+    },
+    {
+        "min_salary": 1000,
+        "max_salary": 2000,
+        "date_posted": "03-10-2021",
+    },
+    {
+        "min_salary": 3000,
+        "max_salary": 6000,
+        "date_posted": "05-10-2021",
+    },
+]
+
+jobs_by_max_salary = [
+    {
+        "min_salary": 1000,
+        "max_salary": 2000,
+        "date_posted": "03-10-2021",
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "04-10-2021",
+    },
+    {
+        "min_salary": 3000,
+        "max_salary": 6000,
+        "date_posted": "05-10-2021",
+    },
+]
+
+jobs_by_min_salary = [
+    {
+        "min_salary": 1000,
+        "max_salary": 2000,
+        "date_posted": "03-10-2021",
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "04-10-2021",
+    },
+    {
+        "min_salary": 3000,
+        "max_salary": 6000,
+        "date_posted": "05-10-2021",
+    },
+]
+
+jobs_by_date_posted = [
+    {
+        "min_salary": 3000,
+        "max_salary": 6000,
+        "date_posted": "05-10-2021",
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 4000,
+        "date_posted": "04-10-2021",
+    },
+    {
+        "min_salary": 1000,
+        "max_salary": 2000,
+        "date_posted": "03-10-2021",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    assert sort_by(all_jobs, "max_salary") == jobs_by_max_salary
+
+    assert sort_by(all_jobs, "min_salary") == jobs_by_min_salary
+
+    assert sort_by(all_jobs, "date_posted") == jobs_by_date_posted

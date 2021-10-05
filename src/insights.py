94c377109d845b8d1c71e8cef8a9e52cdaebe6b2
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-10-04 23:07:32 -0300

    req7

--- src/insights.py
+++ src/insights.py
@@ -35,21 +35,13 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    filtered_jobs = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    for job in jobs:
+        if (job["industry"] == industry):
+            filtered_jobs.append(job)
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return filtered_jobs
 
 
 def get_max_salary(path):

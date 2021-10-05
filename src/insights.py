c8aebe7a57b9b8b50a0040e1882a9bf4f3700739
Julianafatsil <julianafatsil@gmail.com>
2021-10-04 21:19:25 -0300

    Create function filter_by_job_type with for, if

--- src/insights.py
+++ src/insights.py
@@ -11,21 +11,14 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    jobs_list = [
+        job for job in jobs
+        if job["job_type"] == job_type
+    ]
+    return jobs_list
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+#  print(filter_by_job_type(read("src/jobs.csv"), "FULL_TIME"))
 
 
 def get_unique_industries(path):

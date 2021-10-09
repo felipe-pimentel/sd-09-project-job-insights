f0812c7dbc10802da0eac8de596a08308da40c32
ilanribeiro <ilanribeiro@gmail.com>
2021-10-09 18:18:04 -0300

    feat: requirement 2 - ok - get unique job types

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    jobs_list = read(path)
+    set_unique_job = set()
+    for job in jobs_list:
+        set_unique_job.add(job["job_type"])
+    return set_unique_job
 
 
 def filter_by_job_type(jobs, job_type):
